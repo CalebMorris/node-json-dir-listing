7e033fa8f3fab5f43073d3beed953431b0d05c2a
Caleb Morris <caleb.morris.g@gmail.com>
2016-02-08 19:04:05 -0700

    Added test basic to start from

---
+++ test/dirTreeTest.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+
+describe('example base suite', function() {
+
+  describe('example suite', function() {
+
+    it('example test', function(done) {
+
+      expect(0).to.equal(0);
+      done();
+
+    });
+
+  });
+
+});
