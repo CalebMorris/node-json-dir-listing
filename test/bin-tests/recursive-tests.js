0ecb722d64b11288ce20d759658800f796c0429b
Caleb Morris <caleb.morris.g@gmail.com>
2016-02-13 16:15:44 -0700

    Added test to prevent regression from #1

--- test/bin-tests/recursive-tests.js
+++ test/bin-tests/recursive-tests.js
@@ -31,12 +31,14 @@ describe('recursive-tests', function() {
   describe('dir file child dir', function() {
 
     var secondaryPath;
+    var trinaryPath;
 
     beforeEach(function(done) {
       secondaryPath = path.join(basePath, randKey());
+      trinaryPath = path.join(secondaryPath, randKey());
       return fs.mkdir(secondaryPath, function(err) {
         if (err) return done(err);
-        return fs.mkdir(path.join(secondaryPath, randKey()), function(innerErr) {
+        return fs.mkdir(trinaryPath, function(innerErr) {
           if (innerErr) return done(innerErr);
           return fs.writeFile(path.join(basePath, randKey()), 'test data', done);
         });
@@ -57,6 +59,39 @@ describe('recursive-tests', function() {
             expect(exists).to.equal(true);
             return fs.exists(path.join(secondaryPath, defaultListingFile), function(innerExists) {
               expect(innerExists).to.equal(true);
+              done();
+            });
+          });
+        });
+      } catch (err) { done(err); }
+    });
+
+    it('should report two listings', function(done) {
+      try {
+        return binRunner(basePath, ['-R', '-d'], function(report) {
+          expect(report).to.be.an('object');
+          expect(report.path).to.equal(basePath);
+          expect(report.exitCode).to.equal(0);
+          expect(report.stdout).to.be.an('array');
+          expect(report.stdout.length).to.equal(3);
+          try {
+            var baseFiles = [
+              trinaryPath,
+              secondaryPath,
+              basePath,
+            ];
+            for (var i = 0; i < report.stdout.length; i++) {
+              var dryrunReport = JSON.parse(report.stdout[i]);
+              expect(dryrunReport).to.be.an('object');
+              expect(dryrunReport.listingFile).to.equal(path.join(baseFiles[i], defaultListingFile));
+            }
+          } catch (err) { return done(err); }
+          expect(report.stderr).to.be.an('array');
+          expect(report.stderr.length).to.equal(0);
+          return fs.exists(path.join(basePath, defaultListingFile), function(exists) {
+            expect(exists).to.equal(false);
+            return fs.exists(path.join(secondaryPath, defaultListingFile), function(innerExists) {
+              expect(innerExists).to.equal(false);
               done();
             });
           });
